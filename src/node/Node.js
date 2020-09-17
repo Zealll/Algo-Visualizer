aa159333dce589ba3400f6841709d8a26b884e09
Zealll <eriznis@gmail.com>
2020-09-17 04:36:20 -0400

    added more functionality

--- src/node/Node.js
+++ src/node/Node.js
@@ -3,14 +3,21 @@ import React, { useState } from 'react'
 
 const Node = props => {
     const [visited, setVisited] = useState(false)
-    const [lat, setLat] = useState(props.lat)
-    const [lon, setLon] = useState(props.lon)
+    // Longitude up => down
+    // Latitude  left => right
+    const [coor, setCoor] = useState(props.coor)
+    
+    const [start, setStart] = useState(coor === 1930)
+    const [end, setEnd] = useState(coor === 1980)
+    const [distance, setDistance] = useState(start ? 0 : Infinity)
+
+    
 
     // console.log(lat)
     // console.log('hello')
+    // console.log(start, lat, lon)
     return (
-        <div onClick={() => setVisited(!visited)} className={visited ? 'square visited' : 'square'}>
-
+        <div onClick={() => {setVisited(!visited)}} className={visited ? `square visited ${start ? 'start' : ' '} ${end ? 'end' : ' '}` : `square ${start} ${start ? 'start' : ' '} ${end ? 'end' : ' '}`}>
         </div>
     )
 }
