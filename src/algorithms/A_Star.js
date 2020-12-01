8f08f57878f16bdf08431c597a4e798c410813ee
Zealll <eriznis@gmail.com>
2020-11-30 21:47:08 -0500

    Cleaned up indentations

--- src/algorithms/A_Star.js
+++ src/algorithms/A_Star.js
@@ -3,40 +3,40 @@ export function aStar(nodes, startNode, endNode) {
     const closedList = []
     // This is gonna be the F of the A* algo equation
     startNode.setDistance(0)
-    // Uncomment when the loop won't be infinite
     openList.push(startNode)
+
     function recursion() {
-    let currentNode = openList.sort((node1, node2) => node1.distance - node2.distance).shift()
+        let currentNode = openList.sort((node1, node2) => node1.distance - node2.distance).shift()
 
-    closedList.push(currentNode)
+        closedList.push(currentNode)
 
-    if (currentNode === endNode) return 
-    if (!currentNode) return
-    if (currentNode.isWall) return recursion()
+        if (currentNode === endNode) return 
+        if (!currentNode) return
+        if (currentNode.isWall) return recursion()
 
-    currentNode.visit()
-    document.getElementById(`Row-${currentNode.lon}-Col-${currentNode.lat}`).className = `${document.getElementById(`Row-${currentNode.lon}-Col-${currentNode.lat}`).className} visited`
+        currentNode.visit()
+        document.getElementById(`Row-${currentNode.lon}-Col-${currentNode.lat}`).className = `${document.getElementById(`Row-${currentNode.lon}-Col-${currentNode.lat}`).className} visited`
 
-    let neighbors = currentNode.findNeighbors(nodes)
-    for (let n of neighbors) {
-        if (closedList.includes(n)) continue
-     
-        //Replace the "currentNode" with "n" and the path will change!!! 
-        let HDistance = ((currentNode.lon - endNode.lon) ** 2) + ((currentNode.lat - endNode.lat) ** 2)
-        n.h = currentNode.h + 1
-        n.setDistance(n.h + currentNode.weight + HDistance)
+        let neighbors = currentNode.findNeighbors(nodes)
+        for (let n of neighbors) {
+            if (closedList.includes(n)) continue
+        
+            //Replace the "currentNode" with "n" and the path will change!!! 
+            let HDistance = ((currentNode.lon - endNode.lon) ** 2) + ((currentNode.lat - endNode.lat) ** 2)
+            n.h = currentNode.h + 1
+            n.setDistance(n.h + currentNode.weight + HDistance)
 
-        n.prevNode = currentNode
+            n.prevNode = currentNode
 
-        if (!openList.includes(n)) openList.push(n)
+            if (!openList.includes(n)) openList.push(n)
+        }
+
+        setTimeout(() => {
+            return recursion()
+        }, 10)
     }
 
-    setTimeout(() => {
-        return recursion()
-    }, 10)
-}
-
-recursion()
+    recursion()
 }
 
 
